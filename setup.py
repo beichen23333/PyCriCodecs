ec2765905d21b55ad12aea5b3b25d68b9efd0102
北辰 <3857327758@qq.com>
2025-12-26 23:21:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import os
 
 setup(
     name="PyCriCodecs",
-    version="0.4.11",
+    version="0.4.12",
     description="Python frontend with a C++ backend of managing Criware files of all kinds.",
     packages=["PyCriCodecs"],
+    install_requires=[
+        "pydub"
+    ],
     ext_modules=[Extension(
         'CriCodecs',
         [os.path.join("CriCodecs", "CriCodecs.cpp")],
